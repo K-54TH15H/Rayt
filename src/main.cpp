00be679647dcb22b7185a2d16c74132e0fdadfcb
k-54th15h <sathish061006@gmail.com>
2026-01-12 23:46:29 +0530

    progress bar update

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@ int main()
 {
     int img_w = 256;
     int img_h = 256;
-    
+
     // The Below block of code renders the image
     std::cout << "P3" << std::endl << img_w << ' ' << img_h << std::endl << 255 << std::endl;
 
     for(int i = 0; i < img_h; i++)
     {
-	std::clog << "Progress : " << ((((double)i)/(img_h-1)) * 100) << '%' << std::endl;
+	std::clog << '\r';
+	std::clog << std::string(25, ' ') << '\r';
+	std::clog << "Progress : " << ((((double)i)/(img_h-1)) * 100) << '%';
 
 	for(int j = 0; j < img_w; j++)
 	{
@@ -26,5 +28,7 @@ int main()
 	}
     }
 
-    std::clog << "Render Complete" << std::endl;
+    std::clog << std::endl << "Render Complete" << std::endl;
+
+    return 0;
 }
